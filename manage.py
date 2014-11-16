8786395537f334da1fb72d897a4b946f0b924844
sthoma <sthoma4@gmail.com>
2014-11-16 10:49:29 -0700

    Use Flask-Script manager.

---
+++ manage.py
@@ -0,0 +1,33 @@
+import os
+
+if os.path.exists('.env-vars'):
+    print('Importing environment from .env-vars...'),
+    for line in open('.env-vars'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+    print('complete!')
+
+from app import create_app
+
+from flask.ext.script import Manager, Shell
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+
+def make_shell_context():
+    return dict(app=app) # TODO: pass models
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+@manager.command
+def profile(length=25, profile_dir=None):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
+if __name__ == "__main__":
+    manager.run()
+
