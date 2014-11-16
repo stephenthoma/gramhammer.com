f7b979eb0352b9db003e46154bf056b6258500b3
sthoma <sthoma4@gmail.com>
2014-11-16 11:09:12 -0700

    Add unittests command to manager.

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
 import os
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 if os.path.exists('.env-vars'):
     print('Importing environment from .env-vars...'),
     for line in open('.env-vars'):
@@ -19,6 +25,30 @@ def make_shell_context():
     return dict(app=app) # TODO: pass models
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+
+@manager.command
+def test(coverage=False):
+    """Run all unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover(
+        'tests/backend_tests',
+        pattern='test*.py'
+    )
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 @manager.command
 def profile(length=25, profile_dir=None):
